fe7ec1c01ae5ac510c5cc774c057fc7617fb1e33
Elhammoumi <youssef.elhammoumi@estiam.ma>
2020-08-04 17:23:21 +0200

    register/login/logout
    register,login,logout

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginuser, name="login"),
+    path('logout/', views.logoutuser, name="logout"),
+    path('register/', views.registerpage, name="register"),
     path('product/', views.products, name="product"),
     path('customer/<str:pk>', views.customer, name="customer"),
     path('', views.home, name="Home"),
--- account/views.py
+++ account/views.py
@@ -3,7 +3,47 @@ from django.forms import inlineformset_factory # to make a multi_forms fields
 from .models import *
 from .forms import *
 from .filters import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages # that is for showing message if you loging in this work
+from django.contrib.auth import authenticate, login, logout # that is specially for checking user and login/out
+from django.contrib.auth.decorators import login_required #decorator in python allow us to applicate a function in another function 
 
+def registerpage(request):
+    if request.user.is_authenticated:
+        return redirect('Home')
+    else:
+        form = CreateUserform
+        if request.method == 'POST':
+            form = CreateUserform(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'you have successfully registred '+ user)
+                return redirect('login')
+    context = {'form': form}
+    return render(request,'register.html', context)
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect('Home')
+    else:
+        if request.method =='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('Home')
+            else:
+                messages.info(request, 'Username or password is incorrect. Try again')
+    context = {}
+    return render(request,'login.html', context)
+
+def logoutuser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -15,10 +55,12 @@ def home(request):
     context = {'orders':orders, 'customers': customers,'total_customers':total_customers,'total_orders':total_oders,'delivered':delivered,'pending':pending}
     return render(request,"Home.html",context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request,"product.html", {"products": products})
 
+@login_required(login_url='login')
 def customer(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
@@ -28,6 +70,7 @@ def customer(request, pk):
     context = {'customer':customer, 'orders':orders, 'total_orders': total_orders, 'myfilter': myfilter}
     return render(request,"customer.html", context)
 
+@login_required(login_url='login')
 def createorder(request, pk):
     SetOrder = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
     customer = Customer.objects.get(id=pk)
@@ -42,6 +85,7 @@ def createorder(request, pk):
     context = {"form": form}
     return render(request,"orderform.html", context)
 
+@login_required(login_url='login')
 def updateorder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -53,6 +97,7 @@ def updateorder(request, pk):
     context = {'form': form}
     return render(request,"orderform.html", context)
 
+@login_required(login_url='login')
 def deleteorder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
